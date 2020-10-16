cf743c922ccbfb4829a461dced531508f4e69e2f
Vjacheslav Borisov <slavb@ilb.ru>
2020-10-16 07:57:32 +0300

    archtype version

--- archetype/archetype_test.sh
+++ archetype/archetype_test.sh
@@ -1,3 +1,3 @@
 application=${1:-sampleapplication}
 rm -rf ${application}
-mvn -DarchetypeGroupId=ru.ilb.jparestresource -DarchetypeArtifactId=jparestresource-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=ru.ilb.${application} -DartifactId=${application} -Dversion=1.0-SNAPSHOT -Dpackage=ru.ilb.${application} -Darchetype.interactive=false -DarchetypeCatalog=local --batch-mode --update-snapshots archetype:generate
+mvn -DarchetypeGroupId=ru.ilb.jparestresource -DarchetypeArtifactId=jparestresource-archetype -DarchetypeVersion=1.1-SNAPSHOT -DgroupId=ru.ilb.${application} -DartifactId=${application} -Dversion=1.0-SNAPSHOT -Dpackage=ru.ilb.${application} -Darchetype.interactive=false -DarchetypeCatalog=local --batch-mode --update-snapshots archetype:generate
